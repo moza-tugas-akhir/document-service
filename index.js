badfa89229aff49c21d81ed2a2f3b219d7f81266
edobintang <rokatendaedo@gmail.com>
2024-07-29 09:53:10 +0700

    init repo for moza

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+// running express app
+
+// ambil url blockchain service dari env
+
+// generate mockup NIB pdf file
+
+// sending file to bc service using axios -> http post req
